58196de63ca805d4afb426a578f926cd48197e56
Akkari Wassim <wassou93@live.fr>
2019-10-28 11:09:01 +0100

    float conversion

--- Engine/Game.h
+++ Engine/Game.h
@@ -49,11 +49,11 @@ private:
 	MainWindow& wnd;
 	Graphics gfx;
 	/********************************/
-	/*  User Variables              */
+	/*  User Variables              */ 
 	std::random_device rd;
 	std::mt19937 rng;
-	std::uniform_int_distribution<int> xDist;
-	std::uniform_int_distribution<int> yDist;
+	std::uniform_real_distribution<float> xDist;
+	std::uniform_real_distribution<float> yDist;
 	Dude dude;
 	Goal goal;
 	Meter meter;
